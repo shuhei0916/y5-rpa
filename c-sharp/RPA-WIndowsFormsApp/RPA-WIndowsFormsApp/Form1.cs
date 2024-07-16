03b7799cf1df74e60302f73d17abaff3836c6857
shuhei0916 <shuheiito.stc@gmail.com>
2024-07-16 17:43:41 +0900

    added c-sharp directory

---
+++ c-sharp/RPA-WIndowsFormsApp/RPA-WIndowsFormsApp/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RPA_WIndowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //private void label1_Click(object sender, EventArgs e)
+        //{
+
+        //}
+
+        private void btnClickThis_Click(object sender, EventArgs e)
+        {
+            lblHelloWorld.Text = "hello";
+        }
+    }
+}
---
+++ c-sharp/RPA-WIndowsFormsApp/RPA-WIndowsFormsApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace RPA_WIndowsFormsApp
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            //Console.WriteLine("hello world");
+        }
+    }
+}
